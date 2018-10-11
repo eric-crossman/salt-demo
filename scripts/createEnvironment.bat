f33564d231cd1d7e301c234f482a1a1b4e4ee9e6
Eric Crossman <eric_crossman@cox.net>
2018-10-11 15:46:13 -0400

    Added step 2 for vnet.

--- scripts/createEnvironment.bat
+++ scripts/createEnvironment.bat
@@ -4,9 +4,16 @@ REM
 REM     AZURE_REGION
 REM     AZURE_RESOURCE_GROUP_NAME
 REM     AZURE_COMMON_TAGS
+REM     AZURE_VNET_NAME
+REM     AZURE_VNET_PREFIX
+REM     AZURE_VNET_SUBNET_NAME
+REM     AZURE_VNET_SUBNET_PREFIX
 
 REM - ** IMPORTANT ** - Note that the above AZURE_COMMON_TAGS must NOT be
 REM     quoted in order for the CLI call to be properly parsed.
 
-REM * Step 1 - Create a Resource Group to contain our VM objects
-az group create --location %AZURE_REGION% --name %AZURE_RESOURCE_GROUP_NAME% --tags %AZURE_COMMON_TAGS%
+REM Step 1 - Create a Resource Group to contain our VM objects
+call az group create --location %AZURE_REGION% --name %AZURE_RESOURCE_GROUP_NAME% --tags %AZURE_COMMON_TAGS%
+
+REM Step 2 - Create a virtual network with a specific subnet range
+call az network vnet create -g %AZURE_RESOURCE_GROUP_NAME% -n %AZURE_VNET_NAME% --address-prefix %AZURE_VNET_PREFIX% --subnet-name %AZURE_VNET_SUBNET_NAME% --subnet-prefix %AZURE_VNET_SUBNET_PREFIX%
