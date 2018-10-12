ec8ab9ef04c071075cdb5c63c8d613a3186b73f6
Eric Crossman <eric_crossman@cox.net>
2018-10-12 15:22:02 -0400

    Fixed error, resource group is a required argument.

--- scripts/stopEnvironment.bat
+++ scripts/stopEnvironment.bat
@@ -1,7 +1,9 @@
 REM *** Utility script to stop all virtual machines for Salt Demo
+REM *** NOTE: The following environment variables should be set
+REM       AZURE_RESOURCE_GROUP_NAME
 
 REM *** Stop VM 1 - SaltMaster
-call az vm stop --name SaltMaster
+call az vm stop --resource-group %AZURE_RESOURCE_GROUP_NAME% --name SaltMaster
 
 REM *** Stop VM 2 - MediaWiki
-call az vm stop --name MediaWiki
+call az vm stop --resource-group %AZURE_RESOURCE_GROUP_NAME% --name MediaWiki
